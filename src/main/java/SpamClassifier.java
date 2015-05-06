4cc7b6e2d2d3686e744ac4c0681d78762d0d4237
Bennett <bennett@yobennett.com>
2015-05-05 22:23:22 -0700

    init; add hadoop-client

---
+++ src/main/java/SpamClassifier.java
@@ -0,0 +1,11 @@
+import org.apache.hadoop.conf.Configuration;
+
+public class SpamClassifier {
+
+    private Configuration conf;
+
+    public SpamClassifier() {
+        conf = new Configuration();
+    }
+
+}
